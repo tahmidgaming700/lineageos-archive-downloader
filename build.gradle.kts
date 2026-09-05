752b04305204d8979f0f11fe15a8b4616e243a35
tahmidgaming700 <145141456+tahmidgaming700@users.noreply.github.com>
2026-09-05 16:16:54 +0600

    Fix AGP version for Android 16 release build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.application") version "8.10.2" apply false
+    // 8.10.1 is the stable AGP release that supports compileSdk 36 / Android 16.
+    id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
